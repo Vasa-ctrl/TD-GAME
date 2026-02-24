7581d0146c7a779fbb16de34c0887c729ab3383e
bajervac <bajervac@fel.cvut.cz>
2026-02-24 22:40:19 +0100

    feat(towerConfig): tower stats

---
+++ js/config/towersConfig.js
@@ -0,0 +1,52 @@
+// Soubor: js/config/towersConfig.js
+
+export const TowerStats = {
+    ben: {
+        name: "Ben",
+        description: "Great bear warrior and great companion",
+        image: "assets/img/towers/Ben.png",
+        price: 100,
+        damage: 100,
+        range: 100,
+    },
+    viking: {
+        name: "Viking",
+        description: "Smelly viking that fears no one",
+        image: "assets/img/towers/Viking.png",
+        price: 100,
+        damage: 100,
+        range: 100,
+    },
+    knight: {
+        name: "Knight",
+        description: "A fair and virtuous knight at your service",
+        image: "assets/img/towers/Knight.png",
+        price: 100,
+        damage: 100,
+        range: 100,
+    },
+    ninja: {
+        name: "Ninja",
+        description: "Have you seen him?",
+        image: "assets/img/towers/Ninja.png",
+        price: 100,
+        damage: 100,
+        range: 100,
+    },
+    sorcerer: {
+        name: "Sorcerer",
+        description: "He can do more than just card tricks",
+        image: "assets/img/towers/Sorcerer.png",
+        price: 100,
+        damage: 100,
+        range: 100,
+    },
+    deserter: {
+        name: "Deserter",
+        description: "His friends call him a coward, but for the people, he is their only salvation from evil.",
+        image: "assets/img/towers/Deserter.png",
+        price: 100,
+        damage: 100,
+        range: 100,
+    }
+};
